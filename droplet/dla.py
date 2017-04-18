7d0abff9c8487e76fc20f4fd3abe9470a78a7cf3
Samuel Rowlinson <sjrowlinson@virgnmedia.com>
2017-04-18 01:35:09 +0100

    completed basic framework for 2D aggregate simulations

--- droplet/dla.py
+++ droplet/dla.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import numpy as np
 from numpy.random import rand
+import matplotlib.pyplot as plt
 
 class LatticeType(Enum):
     """The geometry of a lattice, determines the motion of particles."""
@@ -15,7 +16,7 @@ class AttractorType(Enum):
     LINE = 4
     PLANE = 5
 
-class DiffusionLimitedAggregate2D(object):
+class DiffusionLimitedAggregate2D:
     """A two-dimensional Diffusion Limited Aggregate (DLA) structure
     with associated properties such as the aggregate stickiness, the
     type of lattice and the type of attractor used.
@@ -45,8 +46,11 @@ class DiffusionLimitedAggregate2D(object):
                 attractor_type == AttractorType.PLANE):
             raise ValueError("Attractor type must be of a 2D topology for 2D aggregates")
         self._attractor_type = attractor_type
-        self.aggregate = np.array()
-        self.__spawn_diam = 8
+        self.aggregate = np.array(0)
+        self.attractor = np.array(0)
+        self.attractor_size = 1
+        self.__boundary_offset = 8
+        self.__spawn_diam = self.__boundary_offset
         self.__max_radius_sqd = 0
     @property
     def stickiness(self):
@@ -78,6 +82,35 @@ class DiffusionLimitedAggregate2D(object):
                 value == AttractorType.PLANE):
             raise ValueError("Attractor type must be of a 2D topology for 2D aggregates")
         self._attractor_type = value
+    @property
+    def x_coords(self):
+        """Returns the x co-ordinates of all particles in the aggregate. This
+        is a readonly property, useful for plotting.
+
+        Returns:
+        --------
+        The x co-ordinates of the aggregate particles.
+        """
+        return self.aggregate[:,0]
+    @property
+    def y_coords(self):
+        """Returns the y co-ordinates of all particles in the aggregate. This
+        is a readonly property, useful for plotting.
+
+        Returns:
+        --------
+        The y co-ordinates of the aggregate particles.
+        """
+        return self.aggregate[:,1]
+    def __initialise_attractor(self):
+        if self._attractor_type == AttractorType.POINT:
+            self.attractor = np.zeros((1,2))
+            return np.arange(1)
+        elif self._attractor_type == AttractorType.LINE:
+            self.attractor = np.zeros((self.attractor_size, 2), dtype=int)
+            self.attractor[:,0] = (np.arange(self.attractor_size)
+                                   - (int)(0.5*self.attractor_size))
+            return np.arange(self.attractor_size)
     def __spawn_brownian_particle(self, crr_pos):
         ppr = rand()
         if self._attractor_type == AttractorType.POINT:
@@ -104,22 +137,53 @@ class DiffusionLimitedAggregate2D(object):
                 crr_pos[1] += 1
             else:
                 crr_pos[1] -= 1
+        elif self.lattice_type == LatticeType.TRIANGLE:
+            if mov_dir < 1.0/6.0:
+                crr_pos[0] += 1
+            elif mov_dir >= 1.0/6.0 and mov_dir < 2.0/6.0:
+                crr_pos[0] -= 1
+            elif mov_dir >= 2.0/6.0 and mov_dir < 3.0/6.0:
+                crr_pos[0] += 1
+                crr_pos[1] += 1
+            elif mov_dir >= 3.0/6.0 and mov_dir < 4.0/6.0:
+                crr_pos[0] += 1
+                crr_pos[0] -= 1
+            elif mov_dir >= 4.0/6.0 and mov_dir < 5.0/6.0:
+                crr_pos[0] -= 1
+                crr_pos[1] -= 1
+            else:
+                crr_pos[0] -= 1
+                crr_pos[1] += 1
     def __lattice_boundary_collision(self, crr_pos, prv_pos):
         epsilon = 2
         if self._attractor_type == AttractorType.POINT:
-            if (np.abs(crr_pos[0]) > (self.__spawn_diam/2 + epsilon) or
-                    np.abs(crr_pos[1]) > (self.__spawn_diam/2 + epsilon)):
-                crr_pos = prv_pos
-    def __aggregate_collision(self, crr_pos, prv_pos):
+            if (np.abs(crr_pos[0]) > (int)(self.__spawn_diam*0.5 + epsilon) or
+                    np.abs(crr_pos[1]) > (int)(self.__spawn_diam*0.5 + epsilon)):
+                crr_pos[:] = prv_pos
+    def __aggregate_collision(self, crr_pos, prv_pos, count, agg_range, att_range):
         if rand() > self._stickiness:
             return False
-        for p in np.nditer(self.aggregate):
-            if p == crr_pos:
-                self.aggregate[-1] = prv_pos
+        # check for collision with attractor seed
+        for idx1 in att_range:
+            if (self.attractor[idx1][0] == crr_pos[0] and
+                    self.attractor[idx1][0] == crr_pos[1]):
+                self.aggregate[count][0] = prv_pos[0]
+                self.aggregate[count][1] = prv_pos[1]
                 radius_sqd = prv_pos[0]**2 + prv_pos[1]**2
                 if radius_sqd > self.__max_radius_sqd:
                     self.__max_radius_sqd = radius_sqd
-                    self.__spawn_diam = radius_sqd + 8
+                    self.__spawn_diam = (int)(np.sqrt(radius_sqd)) + self.__boundary_offset
+                return True
+        # check for collision with aggregate structure
+        for idx2 in agg_range:
+            if (self.aggregate[idx2][0] == crr_pos[0] and
+                    self.aggregate[idx2][1] == crr_pos[1]):
+                self.aggregate[count][0] = prv_pos[0]
+                self.aggregate[count][1] = prv_pos[1]
+                radius_sqd = prv_pos[0]**2 + prv_pos[1]**2
+                if radius_sqd > self.__max_radius_sqd:
+                    self.__max_radius_sqd = radius_sqd
+                    self.__spawn_diam = (int)(np.sqrt(radius_sqd)) + self.__boundary_offset
                 return True
     def generate(self, nparticles, real_time_display=True):
         """Generates an aggregate consisting of `nparticles`.
@@ -127,22 +191,24 @@ class DiffusionLimitedAggregate2D(object):
         Arguments:
         ----------
         - nparticles -- Number of particles in the aggregate.
-        - real_time_display -- Determines whether to plot the aggrgete simulation
+        - real_time_display -- Determines whether to plot the aggregate simulation
         in real time.
         """
-        self.aggregate = np.zeros(nparticles)
+        attrange = self.__initialise_attractor()
+        self.aggregate = np.zeros((nparticles, 2), dtype=int)
+        aggrange = np.arange(nparticles)
         # current co-ordinates
-        current = np.zeros(2)
+        current = np.zeros(2, dtype=int)
         has_next_spawned = False
         count = 0
         while count < nparticles:
             if not has_next_spawned:
                 self.__spawn_brownian_particle(current)
                 has_next_spawned = True
-            previous = current
+            previous = np.empty_like(current)
+            previous[:] = current
             self.__update_brownian_particle(current)
             self.__lattice_boundary_collision(current, previous)
-            if self.__aggregate_collision(current, previous):
+            if self.__aggregate_collision(current, previous, count, aggrange, attrange):
                 count += 1
                 has_next_spawned = False
-            
