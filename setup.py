3a3d9c622ed63f979b7e15ce348df13dbaee56d4
Samuel Rowlinson <sjrowlinson@virgnmedia.com>
2017-05-23 21:40:59 +0100

    minor modifications to setup script

--- setup.py
+++ setup.py
@@ -15,4 +15,21 @@ setup(
     url='https://github.com/SJR276/droplet',
     author='Samuel Rowlinson',
     license='LGPL-3.0',
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+        'Intended Audience :: Science/Research'
+        'Topic :: Scientific/Engineering :: Physics'
+        'Topic :: Scientific/Engineering :: Chemistry'
+        'Topic :: Scientific/Engineering :: Visualization'
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='aggregates diffusion fractals physics visualization',
+    packages=find_packages(exclude=['example_images', 'tests']),
+    install_requires=['numpy', 'matplotlib', 'mpl_toolkits'],
 )
